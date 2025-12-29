f607b9ab4155068f92f3a8831b7ec3c097f52b50
sesetty bhargavi <sesettybhargavi@gmail.com>
2025-12-29 19:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".squares");
+
+squares.forEach(square) => {
+	square.addEventListnener("mouseover", () => {
+		squares.forEach((s) => {
+			if (s != square){
+				s.style.backgroundColor = "#6F4E37";
+			}
+		});
+	});
+	square.addEventListnener("mouseleave", () => {
+		squares.forEach((s) => {
+				s.style.backgroundColor = "#E6E6FA";
+			});
+		});
+	}
